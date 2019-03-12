0d3a57c0bdd1c9f016d389b4ff8a0fff8b59b22b
Isaac Smith <isaac.smitty@gmail.com>
2019-03-12 16:28:47 +0000

    broke up html into react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
+  constructor() {
+    super()
+    
+    this.state = 
+    {
+      currentUser: {name: "Bob"},
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+  }
+
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
---
+++ src/MessageList.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+
+  render() {
+    return (
+        <div>
+            <Message />
+            <Message />
+            <Message />
+        </div>
+    );  
+  }
+}
+
+export default MessageList;
+
+
+
+
+
+
+
